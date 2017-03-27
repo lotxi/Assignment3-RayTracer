fe29060cc62a643842628b1eca0d1c7103d136c8
Tim Gottschalk <tim.gotts@gmail.com>
2017-03-26 22:18:57 -0400

    Cleanup, refactoring, and new test-cases

--- Assignment3-RayTracer/main.cpp
+++ Assignment3-RayTracer/main.cpp
@@ -4,38 +4,41 @@
 #include <cimg/CImg.h>
 #include "Plane.h"
 #include <thread>
-#define MAX_RAY_DEPTH = 5;
+#include <mutex>
 
-const float ambient_light = 1.0;
+
 const int sample_count = 10;
-double accuracy = 0.00001;
-const int max_ray_depth = 10;
-int count = 0;
-glm::vec3 cam_dir = glm::vec3(0, 0, -1);
-glm::vec3 cam_down = glm::vec3(0, -1, 0);
-glm::vec3 cam_right = glm::vec3(1, 0, 0);
+const int thread_count = 10;
+const double accuracy = 0.00001;
+const int max_ray_depth = 4;
+const float ambient_constant = 0.75;
+const float reflectivity_constant = 0.3;
+const glm::vec3 cam_dir = glm::vec3(0, 0, -1);
+const glm::vec3 cam_down = glm::vec3(0, -1, 0);
+const glm::vec3 cam_right = glm::vec3(1, 0, 0);
 
-float saturate(float x)
+// Clamp a float to [0,1]
+float saturate(const float &f)
 {
-	if (x > 255.f)
+	if (f > 1.f)
 	{
-		return 255.f;
+		return 1.f;
 	}
-	if (x < 0)
+	if (f < 0)
 	{
 		return 0.f;
 	}
-	return x;
+	return f;
 }
 
-glm::vec3 saturate(glm::vec3 c)
+// Clamp a vec3 of floats to [0, 255]
+glm::vec3 saturate(const glm::vec3 &color)
 {
-	glm::vec3 result = glm::vec3(saturate(c.x), saturate(c.y), saturate(c.z));
-
-	return result;
+	return glm::vec3(saturate(color.x), saturate(color.y), saturate(color.z));
 }
 
-void writeImage(glm::vec3* pixels, int width, int height)
+// Writes color data from an array of vec3s to a bitmap file
+void writeImage(glm::vec3* pixels, const int &width, const int &height, const std::string &FileName)
 {
 	cimg_library::CImg<float> image(width, height, 1, 3, 0);
 	for (int x = 0; x < width; x++)
@@ -51,19 +54,18 @@ void writeImage(glm::vec3* pixels, int width, int height)
 			image(x, height - y - 1, 0, 2) = blue;
 		}
 	}
-	image.normalize(0, 255);
-	image.save("render.bmp");
-	cimg_library::CImgDisplay main_disp(image, "Render");
+	//image.normalize(0, 255); 
+	image.save((FileName+".bmp").c_str());
+	/*cimg_library::CImgDisplay main_disp(image, "Render");
 	while (!main_disp.is_closed())
 	{
 		main_disp.wait();
-	}
+	}*/
 }
 
 int closest(const std::vector<float>& intersections)
 {
 	int index_min;
-	// Prevent unneeded calculations
 	if (intersections.size() == 0) // No intersections were found
 	{
 		return -1;
@@ -101,105 +103,113 @@ int closest(const std::vector<float>& intersections)
 
 }
 
+// Returns a random float within the specified range (incluisve)
 float getRandomFloat(const float &low, const float &high)
 {
 	return low + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (high - low)));
 }
 
 
-glm::vec3 getColorAt(const glm::vec3& intersection_position, const glm::vec3& intersection_direction, const std::vector<SceneObject*>& objects, const std::vector<Light*>& lights, int index, double accuracy, int depth)
+glm::vec3 getColorAt(const glm::vec3& intersection_position, const glm::vec3& intersection_direction, const std::vector<SceneObject*>& objects, const std::vector<Light*>& lights, const int& index, const int& depth)
 {
-	//Ambient color
-	glm::vec3 color = ambient_light*objects.at(index)->getAmbient();
-
-	glm::vec3 surface_diffuse = objects.at(index)->getDiffuse();
-	glm::vec3 surface_specular = objects.at(index)->getSpecular();
-	float surface_shininess = objects.at(index)->getShininess();
-	glm::vec3 object_normal = normalize(objects.at(index)->getNormalAt(intersection_position));
+	// Add base ambient colour
+	auto final_color = ambient_constant*objects.at(index)->getAmbient();
 
 
-	glm::vec3 final_color = color;
+	auto surface_diffuse = objects.at(index)->getDiffuse();
+	auto surface_specular = objects.at(index)->getSpecular();
+	auto surface_shininess = objects.at(index)->getShininess();
+	auto object_normal = normalize(objects.at(index)->getNormalAt(intersection_position));
+	float minimum_distance;
 
 	if (depth < max_ray_depth && surface_shininess > 0)
 	{
-		//reflection from objects with specular intensity
-		float dot1 = dot(object_normal, -intersection_direction);
-		glm::vec3 scalar1 = dot1 * object_normal;
-		glm::vec3 add1 = scalar1 + intersection_direction;
-		glm::vec3 scalar2 = add1 * 2.f;
-		glm::vec3 add2 = scalar2 - intersection_direction;
-		glm::vec3 reflection_direction = normalize(add2);
-		glm::vec3 reflection_ray_origin = intersection_position + (0.01f*reflection_direction);
+		//Add reflection from objects with shininess
+		auto reflection_direction = normalize((dot(object_normal, -intersection_direction) * object_normal + intersection_direction)*2.f-intersection_direction);
+		auto reflection_ray_origin = intersection_position + (0.01f*reflection_direction);
+
 
 		Ray reflection_ray(reflection_ray_origin, reflection_direction);
-		// Determine the first object the reflection ray intersects first
-		std::vector<float> reflection_intersections;
-		for (int reflection_index = 0; reflection_index < objects.size(); reflection_index++)
+		// Determine the first object the reflection ray intersects with
+		minimum_distance = INFINITY;
+		int closest_index = -1;
+		for (auto reflection_index = 0; reflection_index < objects.size(); reflection_index++)
 		{
-			reflection_intersections.push_back(objects.at(reflection_index)->Intersect(reflection_ray));
-		}
-		int index_closest_reflection = closest(reflection_intersections);
-		if (index_closest_reflection >= 0)
-		{
-
-			if (reflection_intersections.at(index_closest_reflection) > accuracy) {
-				glm::vec3 reflection_position = intersection_position + reflection_intersections.at(index_closest_reflection) * reflection_direction;
-
-
-				glm::vec3 reflection_intersection_color = getColorAt(reflection_position, reflection_direction, objects, lights, index_closest_reflection, accuracy, depth + 1);
-				final_color += reflection_intersection_color*0.25f;//*objects.at(index)->getShininess();
+			float distance = objects.at(reflection_index)->Intersect(reflection_ray);
+			if (distance < minimum_distance && distance > accuracy)
+			{
+				minimum_distance = distance;
+				closest_index = reflection_index;
 			}
-
+			//reflection_intersections.push_back(objects.at(reflection_index)->Intersect(reflection_ray));
 		}
+		//int index_closest_reflection = closest(reflection_intersections);
+		if (closest_index >= 0)
+		{
+			glm::vec3 reflection_position = intersection_position + minimum_distance * reflection_direction;
+			glm::vec3 reflection_intersection_color = getColorAt(reflection_position, reflection_direction, objects, lights, closest_index, depth + 1);
+			final_color += reflection_intersection_color*reflectivity_constant;//*objects.at(index)->getShininess();
+		}
+
 	}
 
-	for (int light_index = 0; light_index < lights.size(); light_index++)
+//	for (auto light_index = 0; light_index < lights.size(); light_index++)
+	for (auto light: lights)
 	{
-		glm::vec3 light_direction = normalize(lights.at(light_index)->getPosition() - intersection_position);
-		float cosine_angle = dot(object_normal, light_direction);
+		auto light_position = light->getPosition();
+		auto light_direction = normalize(light_position - intersection_position);
+		auto cosine_angle = dot(object_normal, light_direction);
+
 		if (cosine_angle > 0)
 		{
 			// Check for shadows
-			bool shadowed = false;
-			float distance_to_light = length(lights.at(light_index)->getPosition() - intersection_position);
-			glm::vec3 shadow_ray_origin = intersection_position + light_direction*(float)0.3;
+			auto shadowed = false;
+			auto distance_to_light = length(light_position - intersection_position);
+			auto shadow_ray_origin = intersection_position + light_direction*(float)0.3;
 			Ray shadow_ray(shadow_ray_origin, light_direction);
+
 			std::vector<float> secondary_intersections;
-			for (int object_index = 0; object_index < objects.size(); object_index++)
+			minimum_distance = INFINITY;
+			auto closest_index = -1;
+			for (auto object_index = 0; object_index < objects.size(); object_index++)
 			{
+				auto distance = objects.at(object_index)->Intersect(shadow_ray);
+				if (distance<minimum_distance && distance > accuracy)
+				{
+					minimum_distance = distance;
+					closest_index = object_index;
+				}
 				secondary_intersections.push_back(objects.at(object_index)->Intersect(shadow_ray));
 			}
-			int index_closest = closest(secondary_intersections);
-			if (index_closest >= 0)
+			if (closest_index >= 0)
 			{
-				if (secondary_intersections[index_closest] > accuracy && secondary_intersections[index_closest] <= distance_to_light)
+				if (minimum_distance <= distance_to_light)
 				{
 					shadowed = true;
 					break;
 				}
 			}
 
-			if (!shadowed) {
-				glm::vec3 addedLight = glm::vec3(0, 0, 0);
+			if (!shadowed) { // The surface is not in shadow, add color from this light source
+				auto addedLight = glm::vec3(0, 0, 0);
+				auto light_color = light->getColor();
+				auto diffuse = surface_diffuse * std::max((float)0, cosine_angle);
+				
+				auto r = glm::normalize(2 * dot(light_direction, object_normal) * object_normal - light_direction);
+				auto rDotL = dot(r, -intersection_direction);
 
-				glm::vec3 diffuse = surface_diffuse * std::max((float)0, cosine_angle);
-
-				glm::vec3 r = glm::normalize(2 * dot(light_direction, object_normal) * object_normal - light_direction);
-				float rDotL = dot(r, -intersection_direction);
+				// If the dot product is not negative, add diffuse and specular component from light
 				if (rDotL > accuracy) {
-					glm::vec3 specular = surface_specular * std::max((float)0, pow(rDotL, surface_shininess));
-
-					//addedLight += lights.at(light_index)->getColor()*(diffuse + specular);
-					addedLight.x += lights.at(light_index)->getColor().x*(diffuse.x + specular.x);
-					addedLight.y += lights.at(light_index)->getColor().y*(diffuse.y + specular.y);
-					addedLight.z += lights.at(light_index)->getColor().z*(diffuse.z + specular.z);
+					auto specular = surface_specular * std::max((float)0, pow(rDotL, surface_shininess));
+					addedLight.x += light_color.x*(diffuse.x + specular.x);
+					addedLight.y += light_color.y*(diffuse.y + specular.y);
+					addedLight.z += light_color.z*(diffuse.z + specular.z);
 				}
-				else
+				else // Add only diffuse component from light
 				{
-					//addedLight += lights.at(light_index)->getColor()*(diffuse);
-					addedLight.x += lights.at(light_index)->getColor().x*diffuse.x;
-					addedLight.y += lights.at(light_index)->getColor().y*diffuse.y;
-					addedLight.z += lights.at(light_index)->getColor().z*diffuse.z;
+					addedLight.x += light->getColor().x*diffuse.x;
+					addedLight.y += light->getColor().y*diffuse.y;
+					addedLight.z += light->getColor().z*diffuse.z;
 				}
 				final_color += addedLight;
 			}
@@ -263,7 +273,7 @@ void computePixels(const int & xMin, const int &  xMax, const int &yMin, const i
 					if (intersections.at(index_closest) > accuracy)
 					{
 						glm::vec3 intersection_position = Cam_ray_origin + (Cam_ray_direction*intersections.at(index_closest));
-						glm::vec3 intersection_color = getColorAt(intersection_position, Cam_ray_direction, scene.objects, scene.lights, index_closest, accuracy, 0);
+						glm::vec3 intersection_color = getColorAt(intersection_position, Cam_ray_direction, scene.objects, scene.lights, index_closest, 0);
 						samples.push_back(intersection_color);
 						/*pixels[index].x = intersection_color.x;
 						pixels[index].y = intersection_color.y;
@@ -297,11 +307,9 @@ void computePixels(const int & xMin, const int &  xMax, const int &yMin, const i
 	}
 }
 
-int main()
+void CreateRender(std::string FileName)
 {
-
-
-	InputReader* input = new InputReader("scene7.txt");
+	InputReader* input = new InputReader(FileName+".txt");
 
 	Scene scene = input->scene;
 
@@ -325,17 +333,83 @@ int main()
 
 	const clock_t begin_time = clock();
 	std::vector<std::thread> threads;
-	threads.push_back(std::thread(computePixels, 0,xMid, 0, yMid, scene, pixels));
+
+	for (int i = 0; i<thread_count; i++)
+	{
+		threads.push_back(std::thread(computePixels, 0, width, i*(height / thread_count), (i + 1)*(height / thread_count), scene, pixels));
+	}
+
+	/*threads.push_back(std::thread(computePixels, 0,xMid, 0, yMid, scene, pixels));
 	threads.push_back(std::thread(computePixels, 0, xMid, yMid, height, scene, pixels));
 	threads.push_back(std::thread(computePixels, xMid, width, 0, yMid, scene,pixels));
-	threads.push_back(std::thread(computePixels, xMid, width, yMid, height, scene,pixels));
+	threads.push_back(std::thread(computePixels, xMid, width, yMid, height, scene,pixels));*/
 	//void computePixels(glm::vec3* pixels, const int & xMin, const int &  xMax, const Scene &scene)
 
 	std::cout << "synchronizing all threads...\n";
 	for (auto& th : threads) th.join();
 
 	std::cout << "Reached end of raycast loop after " << float(clock() - begin_time) / CLOCKS_PER_SEC << std::endl;
-	writeImage(pixels, width, height);
+	writeImage(pixels, width, height, FileName);
+	
+	//delete pixels;
+	//delete input;
+	return;
+}
+
+int main()
+{
+
+	CreateRender("scene6");
+	std::cout << "Scene 6 complete" << std::endl;
+	CreateRender("scene7");
+	std::cout << "Scene 7 complete" << std::endl;
+	CreateRender("scene8");
+	std::cout << "Scene 8 complete" << std::endl;
+	CreateRender("scene9");
+	std::cout << "Scene 9 complete" << std::endl;
+	CreateRender("scene10");
+	std::cout << "Scene 10 complete" << std::endl;
+	//InputReader* input = new InputReader("scene1.txt");
+
+	//Scene scene = input->scene;
+
+	//int width = scene.width;
+	//int height = scene.height;
+	//int numPixels = width * height;
+	//glm::vec3* pixels = new glm::vec3[numPixels];
+	////glm::vec3* pixels = new glm::vec3[numPixels];
+
+	//float aspect_ratio = scene.camera->getAspectRatio();
+	//glm::vec3 Cam_ray_origin = scene.camera->getPosition();
+	//float fov = glm::radians(scene.camera->getFoV());
+
+
+
+	//srand(static_cast <unsigned>(time(0)));
+	//int xMid, yMid;
+	//xMid = ceil(width / 2);
+	//yMid = ceil(height / 2);
+
+
+	//const clock_t begin_time = clock();
+	//std::vector<std::thread> threads;
+
+	//for (int i=0; i<thread_count; i++)
+	//{
+	//	threads.push_back(std::thread(computePixels, 0, width, i*(height/thread_count), (i+1)*(height/thread_count), scene, pixels));
+	//}
+
+	///*threads.push_back(std::thread(computePixels, 0,xMid, 0, yMid, scene, pixels));
+	//threads.push_back(std::thread(computePixels, 0, xMid, yMid, height, scene, pixels));
+	//threads.push_back(std::thread(computePixels, xMid, width, 0, yMid, scene,pixels));
+	//threads.push_back(std::thread(computePixels, xMid, width, yMid, height, scene,pixels));*/
+	////void computePixels(glm::vec3* pixels, const int & xMin, const int &  xMax, const Scene &scene)
+
+	//std::cout << "synchronizing all threads...\n";
+	//for (auto& th : threads) th.join();
+
+	//std::cout << "Reached end of raycast loop after " << float(clock() - begin_time) / CLOCKS_PER_SEC << std::endl;
+	//writeImage(pixels, width, height);
 
 	return 0;
 
